fc52c212cef20ebe3540210a154daeabef75e6a9
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-01-30 18:39:23 +0100

    task 4 is completed

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,52 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  private tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(tenant: Person): void {
+    if (this.door) {
+      this.tenants.push(tenant);
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log(`Door is opened, please come in!`);
+    } else {
+      console.log(`You don't have proper key, you can't open the door!`);
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +55,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
